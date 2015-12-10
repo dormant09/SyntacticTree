67278c03419e52aca0c284048d930d3e81f03704
Sijoon Kim <dormant09@snu.ac.kr>
2015-12-10 20:03:04 +0900

    Initial Commit

---
+++ SyntacticTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SyntacticTree
+//
+//  Created by Sijoon Kim on 2015. 12. 10..
+//  Copyright © 2015년 Sijoon Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
